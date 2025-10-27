ba6dff0fa4d2cbfe62f70d9864264babc90c4b13
Artem <Artem@gamil.com>
2025-10-27 20:18:05 +0200

    313

--- Project2/Source.cpp
+++ Project2/Source.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <cstdlib> 
 using namespace std;
 class Point
 {
@@ -24,25 +25,39 @@ public:
 	Numbers() {	a = 0;	b = 0;}
 	Numbers(T1 _a, T2 _b) { a = _a; b = _b; }
 	void Init(T1 _a, T2 _b){a = _a;b = _b;}
-	void InitRandom(){a =  rand() % 100;b =  rand() % 100;}
-	void Output() { cout << a << "\t" << b<< "\n"; }
+	void InitRandom();
+	void Output();
 	void SetNumber(T1 c, T2 n) { a = c; b = n; }
 	T1 GetA() { return a; }
 	T2 GetB() { return b; }
 };
+template<class T1, class T2>
+void Numbers<T1, T2>::InitRandom()
+{
+	a = rand() % 100;
+	b = rand() % 100;
+}
+
+template<class T1, class T2>
+void Numbers<T1, T2>::Output()
+{
+	cout << a << "\t" << b << endl;
+}
+
+
 template<>
-void Numbers<Point, Point>::InitRandom() {
+void Numbers<Point, Point>::InitRandom()
+{
 	a = Point(rand() % 100, rand() % 100);
 	b = Point(rand() % 100, rand() % 100);
 }
 
 template<>
-void Numbers<Point, Point>::Output() {
-	a.Output(); b.Output();
-}
+void Numbers<Point, Point>::Output() {a.Output();b.Output();}
 
 int main()
 {
+	srand(time(0));
 	Numbers<int, double> obj1(10, 2.5);
 	obj1.Output();
 	Numbers<Point, Point> obj2(Point(1, 2), Point(3, 4));
